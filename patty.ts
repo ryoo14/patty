75f0ccdd48cf07595b66596768061ad9a7f7bdf6
ryoo14 <anana12185@gmail.com>
2022-10-25 23:06:40 +0900

    refactor: add error handle when not defined PATTY_ROOT and HOME

--- patty.ts
+++ patty.ts
@@ -1,6 +1,7 @@
+import dir from "https://deno.land/x/dir@1.5.1/mod.ts";
 import { Command, HelpCommand } from "https://deno.land/x/cliffy@v0.25.2/command/mod.ts";
-import { join } from "https://deno.land/std/path/mod.ts";
-import { ensureDir } from "https://deno.land/std/fs/ensure_dir.ts";
+import { ensureDir } from "https://deno.land/std@0.160.0/fs/mod.ts";
+import { join } from "https://deno.land/std@0.160.0/path/mod.ts";
 
 
 new Command()
@@ -25,9 +26,16 @@ new Command()
   .parse();
 
 const getPattyRoot = () => {
-  const pattyRoot = Deno.env.get("PATTY_ROOT");
-  return pattyRoot ? pattyRoot : Deno.env.get("HOME") + "/patty";
-}
+  const home = dir("home");
+  const homePattyRoot = home ? join(home, "patty") : undefined;
+  const pattyRoot = Deno.env.get("PATTY_ROOT") ?? homePattyRoot;
+  if (!pattyRoot) {
+    console.error("Not defined $PATTY_ROOT and $HOME.");
+    Deno.exit(1);
+  } else {
+    return pattyRoot;
+  }
+};
 
 const create = (dir: string) => {
   const targetDir = join(getPattyRoot(), dir, ".patty");
