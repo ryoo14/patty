d74d4558c821dc02b397c216892cdc4f7792ca97
ryoo14 <anana12185@gmail.com>
2022-11-10 11:57:53 +0900

    test: add only import part
    referring to [hasundue/nublar](https://github.com/hasundue/nublar)

---
+++ test.ts
@@ -0,0 +1,7 @@
+import {
+  assertEquals,
+  assertMatch,
+  assertNotMatch,
+} from "https://deno.land/std@0.163.0/testing/asserts.ts";
+import { $, CommandBuilder } from "https://deno.land/x/dax@0.15.0/mod.ts";
+
