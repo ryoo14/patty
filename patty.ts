3aa91768da5016f45e9c84468c5ce0aa53a5bf15
ryoo14 <anana12185@gmail.com>
2022-11-12 01:16:40 +0900

    chore: bump version to 0.1.5

--- patty.ts
+++ patty.ts
@@ -5,7 +5,7 @@ import { dirname, join, relative } from "https://deno.land/std@0.163.0/path/mod.
 
 new Command()
   .name("patty")
-  .version("0.1.4")
+  .version("0.1.5")
   .description("a CLI tool for managing git and working directories written in Deno.")
   .default("help")
   // Create
