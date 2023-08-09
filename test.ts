ef264abd11457ea2f871d2bef914796e33fb2c0e
denopendabot <denopendabot@github.com>
2023-08-09 01:17:01 +0000

    build(deps): bump dax from 0.33.0 to 0.34.0

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { assertEquals, assertMatch, assertNotEquals } from "https://deno.land/std@0.197.0/testing/asserts.ts";
-import { CommandBuilder } from "https://deno.land/x/dax@0.33.0/mod.ts";
+import { CommandBuilder } from "https://deno.land/x/dax@0.34.0/mod.ts";
 
 function pattyTest(
   testName: string,
